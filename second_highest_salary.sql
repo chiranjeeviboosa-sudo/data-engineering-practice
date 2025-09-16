28728498c33f502517be257c93e42bbdd890a484
chiranjeeviboosa-sudo <chiranjeevi.boosa@gmail.com>
2025-09-16 20:37:15 +0530

    Update second_highest_salary.sql
    additional query

--- second_highest_salary.sql
+++ second_highest_salary.sql
@@ -1,3 +1,11 @@
 SELECT MAX(salary) AS second_highest_salary
 FROM employee
 WHERE salary < (SELECT MAX(salary) FROM employee);
+
+SELECT 
+    student_id,
+    name,
+    marks,
+    RANK() OVER (ORDER BY marks DESC) AS rank
+FROM 
+    students;
